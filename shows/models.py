3e153df26c6e6877ac0d32c3831d2ea0794b9ff4
ASaparbek <TELIDAS>
2022-04-19 15:28:37 +0600

    second lesson

---
+++ shows/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class TVShow(models.Model):
+    GENRE_CHOICE = (
+        ("Comedy", "COMEDY"),
+        ("Action", "ACTION"),
+        ("Romantic", "ROMANTIC"),
+        ("Horror", "HORROR"),
+        ("Anime", "ANIME"),
+        ("Fantastic", "FANTASTIC"),
+    )
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    image = models.ImageField(upload_to="")
+    quantity = models.PositiveIntegerField()
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICE)
+    duration = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ShowComment(models.Model):
+    text = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    shows = models.ForeignKey(TVShow,
+                              on_delete=models.CASCADE,
+                              related_name="shows_comment")
+
+    def __str__(self):
+        return self.shows.title
---
+++ shows/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('shows/', views.shows_all, name="shows_all"),
+    path('shows/<int:id>/', views.shows_detail, name="shows_detail")
+]
