e8d191c73e157d99d40bf6f1da3dc5c361e1501d
ASaparbek <TELIDAS>
2022-04-21 21:46:51 +0600

    third lesson

--- shows/urls.py
+++ shows/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'shows'
 urlpatterns = [
     path('shows/', views.shows_all, name="shows_all"),
-    path('shows/<int:id>/', views.shows_detail, name="shows_detail")
+    path('shows/<int:id>/', views.shows_detail, name="shows_detail"),
+    path('add-show/', views.add_show, name="add_show")
 ]
--- shows/views.py
+++ shows/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
-
+from . import models, forms
+from django.shortcuts import redirect, reverse
 
 def shows_all(request):
     shows = models.TVShow.objects.all()
@@ -10,3 +11,16 @@ def shows_all(request):
 def shows_detail(request, id):
     show = get_object_or_404(models.TVShow, id=id)
     return render(request, "shows_detail.html", {"show": show})
+
+
+def add_show(request):
+    method = request.method
+    if method == "POST":
+        form = forms.TVShowForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # return HttpResponse("Show created successfully")
+            return redirect(reverse("shows:shows_all"))
+    else:
+        form = forms.TVShowForm()
+    return render(request, "add_shows.html", {'form': form})
