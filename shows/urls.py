dc2c67b59935684694911dc8f0a34bfb6ce5bc28
ASaparbek <TELIDAS>
2022-04-26 15:59:31 +0600

    fourth lesson

--- shows/urls.py
+++ shows/urls.py
@@ -5,6 +5,15 @@ from . import views
 app_name = 'shows'
 urlpatterns = [
     path('shows/', views.shows_all, name="shows_all"),
+    path('shows/latest/', views.shows_latest, name="shows_latest"),
+    path('shows/year/', views.shows_in_one_year, name="shows_year"),
+    path('shows/anime/', views.shows_genre_anime, name="shows_anime"),
+    path('shows/action/', views.shows_genre_action, name="shows_action"),
+    path('shows/romantic/', views.shows_genre_romantic, name="shows_romantic"),
+    path('shows/horror/', views.shows_genre_horror, name="shows_horror"),
+    path('shows/fantastic/', views.shows_genre_fantastic, name="shows_fantastic"),
     path('shows/<int:id>/', views.shows_detail, name="shows_detail"),
+    path('shows/<int:id>/update/', views.put_update_shows, name="shows_update"),
+    path('shows/<int:id>/delete/', views.shows_delete, name="shows_delete"),
     path('add-show/', views.add_show, name="add_show")
 ]
--- shows/views.py
+++ shows/views.py
@@ -2,9 +2,48 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from . import models, forms
 from django.shortcuts import redirect, reverse
+from datetime import datetime, timedelta
+
+start_date = datetime.today() - timedelta(days=5)
+one_year_date = datetime.today() - timedelta(days=365)
+
 
 def shows_all(request):
-    shows = models.TVShow.objects.all()
+    shows = models.TVShow.objects.order_by("-id")
+    return render(request, "shows_list.html", {"shows": shows})
+
+
+def shows_latest(request):
+    shows = models.TVShow.objects.filter(created_date__gt=start_date).order_by("-id")
+    return render(request, "shows_list.html", {"shows": shows})
+
+def shows_in_one_year(request):
+    shows = models.TVShow.objects.filter(created_date__gt=one_year_date).order_by("-id")
+    return render(request, "shows_list.html", {"shows": shows})
+
+
+def shows_genre_anime(request):
+    shows = models.TVShow.objects.filter(genre="Anime").order_by("-id")
+    return render(request, "shows_list.html", {"shows": shows})
+
+
+def shows_genre_action(request):
+    shows = models.TVShow.objects.filter(genre="Action").order_by("-id")
+    return render(request, "shows_list.html", {"shows": shows})
+
+
+def shows_genre_romantic(request):
+    shows = models.TVShow.objects.filter(genre="Romantic").order_by("-id")
+    return render(request, "shows_list.html", {"shows": shows})
+
+
+def shows_genre_horror(request):
+    shows = models.TVShow.objects.filter(genre="Horror").order_by("-id")
+    return render(request, "shows_list.html", {"shows": shows})
+
+
+def shows_genre_fantastic(request):
+    shows = models.TVShow.objects.filter(genre="Fantastic").order_by("-id")
     return render(request, "shows_list.html", {"shows": shows})
 
 
@@ -24,3 +63,23 @@ def add_show(request):
     else:
         form = forms.TVShowForm()
     return render(request, "add_shows.html", {'form': form})
+
+
+def put_update_shows(request, id):
+    show_id = get_object_or_404(models.TVShow, id=id)
+    if request.method == "POST":
+        form = forms.TVShowForm(instance=show_id,
+                                data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("shows:shows_all"))
+    else:
+        form = forms.TVShowForm(instance=show_id)
+    return render(request, "shows_update.html", {"form": form,
+                                                 "show": show_id})
+
+
+def shows_delete(request, id):
+    show_id = get_object_or_404(models.TVShow, id=id)
+    show_id.delete()
+    return redirect(reverse("shows:shows_all"))
